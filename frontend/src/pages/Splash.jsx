8c02b4a526e1cec660fc51b5330d4da1eb48543d
Helene <helene.blanche@hotmail.fr>
2023-02-22 15:17:09 +0100

    resolved bugs

--- frontend/src/pages/Splash.jsx
+++ frontend/src/pages/Splash.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import Home from "./Home"
-import porte_droite from "../assets/Images/porte_droite_3.png"
-import porte_gauche from "../assets/Images/porte_gauche_3.png"
+import porteDroite from "../assets/Images/porte_droite_3.png"
+import porteGauche from "../assets/Images/porte_gauche_3.png"
 
 export default function Splash() {
   const [isOpen, setIsOpen] = useState(false)
@@ -16,10 +16,10 @@ export default function Splash() {
   return (
     <div className={`door-container ${isOpen ? "open" : ""}`}>
       <div className="door-left">
-        <img className="left" src={porte_gauche} alt="Porte gauchee" />
+        <img className="left" src={porteGauche} alt="Porte gauchee" />
       </div>
       <div className="door-right">
-        <img className="right" src={porte_droite} alt="Porte droite" />
+        <img className="right" src={porteDroite} alt="Porte droite" />
       </div>
       {
         <div className="splash-home">
