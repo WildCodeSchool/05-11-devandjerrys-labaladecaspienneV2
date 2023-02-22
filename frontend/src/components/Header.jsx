8f9f8a5ee489ac332e3a81edfb0a35f26ad809e4
CCeciliaDev <ceciliadev@outlook.com>
2023-02-22 08:00:41 +0100

    update header with link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/logo_baladecaspienne.png"
 import star from "../assets/brown_star.png"
 import angleL from "../assets/white_angle_L.png"
 import angleR from "../assets/white_angle_R.png"
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
@@ -10,15 +11,23 @@ function Header() {
       <div className="mainDivHeader">
         <img className="headerAngle angleL" src={angleL} alt="image" />
         <img className="headerStar" src={star} alt="image" />
-        <p className="itemsNavHeader">Boutique</p>
+        <Link to="/artifacts">
+          <p className="itemsNavHeader">Boutique</p>
+        </Link>
         <img className="headerStar" src={star} alt="image" />
         <p className="itemsNavHeader">Qui suis-je ?</p>
-        <div className="divLogoHeader">
-          <img className="logoHeader" src={logo} alt="image" />
-        </div>
-        <p className="itemsNavHeader">Connexion</p>
+        <Link to="/home">
+          <div className="divLogoHeader">
+            <img className="logoHeader" src={logo} alt="image" />
+          </div>
+        </Link>
+        <Link to="#">
+          <p className="itemsNavHeader">Connexion</p>
+        </Link>
         <img className="headerStar" src={star} alt="image" />
-        <p className="itemsNavHeader">Panier</p>
+        <Link to="#">
+          <p className="itemsNavHeader">Panier</p>
+        </Link>
         <img className="headerStar" src={star} alt="image" />
         <img className="headerAngle angleR" src={angleR} alt="image" />
       </div>
