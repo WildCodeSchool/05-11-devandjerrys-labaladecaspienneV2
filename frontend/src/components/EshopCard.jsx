a34146e0116c3675a33badbfc3f25dd2f2f94758
Helene <helene.blanche@hotmail.fr>
2023-02-24 20:52:53 +0100

    new changes on the splash page

--- frontend/src/components/EshopCard.jsx
+++ frontend/src/components/EshopCard.jsx
@@ -1,18 +1,33 @@
 import { useEffect, useState } from "react"
 
 import CadreEshop from "../assets/Images/CadreEshop.png"
-import etiquette from "../assets/Images/etiquette.png"
-export default function EshopCard({ id, price, image }) {
-  const [isSeleted, setIsSelected] = useState(false)
+import Artipicture from "../assets/Images/Artifact.png"
+
+export default function EshopCard({ id, price, image, name_arti }) {
+  // const [isSeleted, setIsSelected] = useState([])
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:5000/atifacts")
+  //     .then((res) => setIsSelected(res.data))
+  //     .catch((err) => console.error(err))
+  // }, [])
   return (
     <div className="Eshop">
-      <img className="Frame" src={CadreEshop} alt="cadre de l'image" />
-      <div className="Price">
-        <img
-          className="Eshop-Price"
-          src={etiquette}
-          alt="etiquette, prix de l'artéfact"
-        />
+      <div className="Arti-name">
+        <h2>
+          {" "}
+          Coffre Cthulhu
+          {/* {name_arti} */}
+        </h2>
+      </div>
+      <div className="Image">
+        <img className="Frame" src={CadreEshop} alt="cadre de l'image" />
+        {/* <img className="Arti-photo" src={Artipicture} /> */}
+      </div>
+      <div className="Eshop-Price">
+        <div className="price">
+          <p> 100 €{/* {price} */}</p>
+        </div>
       </div>
     </div>
   )
