8e672e76772c84d04d026d1b05fb8df3b73272b8
Helene <helene.blanche@hotmail.fr>
2023-02-26 13:23:28 +0100

    modification of the backend on the line 10 of ArtifactsControllers.js and on EshopCard.jsx, Artifacts.jsx, EshopCard.scss, splash.scss

--- frontend/src/components/EshopCard.jsx
+++ frontend/src/components/EshopCard.jsx
@@ -1,32 +1,29 @@
-// import { useEffect, useState } from "react"
-
 import CadreEshop from "../assets/Images/CadreEshop.png"
+import Star from "../assets/Images/brown_star.png"
 
-export default function EshopCard() {
-  // { id, price, image, name_arti }
-  // const [isSeleted, setIsSelected] = useState([])
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:5000/atifacts")
-  //     .then((res) => setIsSelected(res.data))
-  //     .catch((err) => console.error(err))
-  // }, [])
+export default function EshopCard(props) {
   return (
     <div className="Eshop">
       <div className="Arti-name">
         <h2>
-          {" "}
-          Coffre Cthulhu
-          {/* {name_arti} */}
+          {/* {" "}
+          Coffre Cthulhu */}
+          {props.name_arti}
         </h2>
       </div>
-      <div className="Image">
-        <img className="Frame" src={CadreEshop} alt="cadre de l'image" />
-        {/* <img className="Arti-photo" src={Artipicture} /> */}
-      </div>
+      <section className="Image">
+        <div
+          className="ArtImage"
+          style={{ backgroundImage: `url(${props.images})` }}
+        >
+          <img className="Frame" src={CadreEshop} alt={props.name_arti} />
+          <img className="E-Star" src={Star} alt="étoile" />
+        </div>
+      </section>
+
       <div className="Eshop-Price">
         <div className="price">
-          <p> 100 €{/* {price} */}</p>
+          <p>{props.price} €</p>
         </div>
       </div>
     </div>
