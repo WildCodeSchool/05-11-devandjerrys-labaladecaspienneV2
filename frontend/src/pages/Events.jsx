24f1197d5046a2101563d6e5cf6702d7d9701d79
Newaluigi <alexandre.levillain@hotmail.com>
2023-03-02 14:26:00 +0100

    some fix on structure

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -10,7 +10,7 @@ import axios from "axios"
 // import { Link } from "react-router-dom"
 import CardEvent from "../components/CardEvent"
 
-export default function Home() {
+export default function Events() {
   const [events, setEvents] = useState([])
   useEffect(() => {
     axios.get("http://localhost:5000/events").then((response) => {
@@ -22,10 +22,14 @@ export default function Home() {
       <Header />
       <div className="EventsBody">
         <h1 className="headTitle">LA BALADE CASPIENNE</h1>
-        <img id="image1" className="image" src={deco}></img>
-        <img id="image2" className="image" src={deco1}></img>
+        <img id="lDesign1" className="ligneDesign" src={deco}></img>
+        <img id="lDesign2" className="ligneDesign" src={deco1}></img>
         <h2 className="eventTitle">Evènements à venir</h2>
-        <img id="image3" className="image" src={deco1}></img>
+        <img id="lDesign3" className="ligneDesign" src={deco1}></img>
+        <div id="introEvent">
+          Pour faire la lumière sur les prochains évènements ou conventions où
+          vous pouvez me retrouver, cliquez sur la bougie...
+        </div>
         <div className="card-event">
           {events.map((event) => (
             <CardEvent
@@ -37,7 +41,7 @@ export default function Home() {
         </div>
         <div className="eventsHistory">
           <h3 className="eventTitle">Evènements passés</h3>
-          <img className="lineTitleEshop" src={LineTop} alt="image" />
+          <img className="lineTitleEvent" src={LineTop} alt="image" />
         </div>
       </div>
       <div className="holder">
