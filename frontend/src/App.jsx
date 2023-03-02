dadbd936313435ab75713cf5fb904d562b73b04d
Noiramd31 <marion2macedo@gmail.com>
2023-03-02 18:47:47 +0100

    cartPage step1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Home from "./pages/Home"
 import Splash from "./pages/Splash"
 import ThemeDetails from "./pages/ThemeDetails"
 import Theme from "./pages/Themes"
+import Cart from "./pages/Cart"
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/artifacts/:id" element={<ArtifactDetails />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/eshop" element={<EshopCard />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   )
