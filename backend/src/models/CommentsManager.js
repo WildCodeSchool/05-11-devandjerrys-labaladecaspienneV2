d4f77de6efa8cd9177cee308bbaf2a1aba903b9c
Helene <helene.blanche@hotmail.fr>
2023-03-07 16:00:51 +0100

    Creation of routes for the order and add new branch 1002_back_order

--- backend/src/models/CommentsManager.js
+++ backend/src/models/CommentsManager.js
@@ -18,6 +18,31 @@ class CommentsManager extends AbstractManager {
       [comment.content, comment.date_create, comment.date_update]
     )
   }
+
+  findAllComments() {
+    return this.database.query(
+      `SELECT content, nickname, comments.date_create FROM ${this.table}
+      JOIN orders ON comments_id = comments.id
+      JOIN users ON users_id = users.id;`
+    )
+  }
+
+  findOneComments(id) {
+    return this.database.query(
+      `SELECT c.id, content, nickname, c.date_create 
+      FROM ${this.table} AS c
+      JOIN orders AS o 
+      ON o.comments_id = c.id
+      JOIN users AS u 
+      ON o.users_id = u.id
+      HAVING id=?`,
+      [id]
+    )
+  }
+
+  deleteComments(id) {
+    return this.database.query(`DELETE FROM comments WHERE id = ?`, [id])
+  }
 }
 
 module.exports = CommentsManager
