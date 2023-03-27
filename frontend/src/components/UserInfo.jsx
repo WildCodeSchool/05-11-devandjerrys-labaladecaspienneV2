1760ed9de0c6e9b9c0ad0a753e3885831d634d9d
SAMY COLLIARD <samy3876@gmail.com>
2023-03-27 10:41:19 +0200

    commit for pull dev

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -5,6 +5,13 @@ import CoinBD from "../assets/coinBD.svg"
 import Separateur from "../assets/Images/separateur.png"
 
 export default function UserInfo(props) {
+  const { userData, onSubmit } = props
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    onSubmit(event)
+  }
+
   return (
     <div className="allUserProfil">
       <div className="userInfo">
@@ -13,43 +20,54 @@ export default function UserInfo(props) {
           <img className="coinHG" src={CoinHG} />
           <img className="coinHD" src={CoinHD} />
         </div>
-        <div className="allEncart">
-          <div className="encart">
-            <div className="encartSousTitre">
-              <h4 className="sousTitre"> Nom de Famille : </h4>
-              <input></input>
+        <form onSubmit={handleFormSubmit}>
+          <div className="allEncart">
+            <div className="encart">
+              <div className="encartSousTitre">
+                <h4 className="sousTitre"> Nom de Famille : </h4>
+                {/* <input
+                  type="text"
+                  name="firstname"
+                  id="firstname"
+                  value={userData.lastname}
+                  placeholder={userData.lastname}
+                  onChange={(event) =>
+                    setUserData({ ...userData, lastname: event.target.value })
+                  }
+                /> */}
+              </div>
+              <img className="separateur" src={Separateur} />
             </div>
-            <img className="separateur" src={Separateur} />
-          </div>
-          <div className="encart">
-            <div className="encartSousTitre">
-              <h4 className="sousTitre"> Prenom : </h4>
-              <input></input>
+            <div className="encart">
+              <div className="encartSousTitre">
+                <h4 className="sousTitre"> Prenom : </h4>
+                <input defaultValue={userData.firstname} />
+              </div>
+              <img className="separateur" src={Separateur} />
             </div>
-            <img className="separateur" src={Separateur} />
-          </div>
-          <div className="encart">
-            <div className="encartSousTitre">
-              <h4 className="sousTitre"> Adresse : </h4>
-              <input></input>
+            <div className="encart">
+              <div className="encartSousTitre">
+                <h4 className="sousTitre"> Adresse : </h4>
+                <input defaultValue={userData.adress_delivery} />
+              </div>
+              <img className="separateur" src={Separateur} />
             </div>
-            <img className="separateur" src={Separateur} />
-          </div>
-          <div className="encart">
-            <div className="encartSousTitre">
-              <h4 className="sousTitre"> Code Postal : </h4>
-              <input></input>
+            <div className="encart">
+              <div className="encartSousTitre">
+                <h4 className="sousTitre"> Code Postal : </h4>
+                <input defaultValue={userData.zip_delivery} />
+              </div>
+              <img className="separateur" src={Separateur} />
             </div>
-            <img className="separateur" src={Separateur} />
-          </div>
-          <div className="encart">
-            <div className="encartSousTitre">
-              <h4 className="sousTitre"> Ville : </h4>
-              <input></input>
+            <div className="encart">
+              <div className="encartSousTitre">
+                <h4 className="sousTitre"> Ville : </h4>
+                <input defaultValue={userData.city_delivery} />
+              </div>
+              <img className="separateur" src={Separateur} />
             </div>
-            <img className="separateur" src={Separateur} />
           </div>
-        </div>
+        </form>
         <div className="coinBas">
           <img className="coinBG" src={CoinBG} />
           <img className="coinBD" src={CoinBD} />
--- frontend/src/pages/UserAccountSecond.jsx
+++ frontend/src/pages/UserAccountSecond.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { useParams } from "react-router-dom"
 import Commande from "../components/Commande"
 import UserEmail from "../components/UserEmail"
 import UserInfo from "../components/UserInfo"
@@ -9,10 +12,44 @@ import Vertical from "../assets/Images/separateurOcreVertical.svg"
 import Horizontal from "../assets/Images/separateurOcreHorizontal.svg"
 
 export default function UserAccount() {
+  const { id } = useParams()
+  const [userData, setUserData] = useState([])
+  const [setUserOrder] = useState([])
+  const [setIsEditing] = useState(false)
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/users/${id}`).then((response) => {
+      setUserData(response.data)
+    })
+  }, [id])
+  useEffect(() => {
+    axios.get(`http://localhost:5000/users/${id}/orders`).then((response) => {
+      setUserOrder(response.data)
+      // console.log(userOrder)
+    })
+  }, [id])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    axios
+      .put(`http://localhost:5000/users/${id}`, userData)
+      .then((response) => {
+        // console.log("Poulette", response.data)
+        setUserData(response.data)
+        setIsEditing(false)
+      })
+      .catch((error) => {
+        console.error("Erreur :", error)
+      })
+
+    axios.get(`http://localhost:5000/users/${id}`).then((response) => {
+      setUserOrder(response.data)
+    })
+  }
   return (
     <div>
       <Header />
-      <div className="pageUser">
+      <div className="pageUser" key={userData.id}>
         <div className="titrePage">
           <h2 className="titre">LA BALADE CAPSIENNE</h2>
           <img src={Headline} className="headLine" />
@@ -23,7 +60,7 @@ export default function UserAccount() {
               <UserEmail />
             </div>
             <div>
-              <UserInfo />
+              <UserInfo userData={userData} onSubmit={handleSubmit} />
             </div>
           </div>
           <img src={Vertical} className="vertical" />
