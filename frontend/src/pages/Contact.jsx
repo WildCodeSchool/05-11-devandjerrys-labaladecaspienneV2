ccf5acea71587fe148f9a5d0ae4b64beafbb3842
Newaluigi <alexandre.levillain@hotmail.com>
2023-03-29 17:21:35 +0200

    commit before contact branch creation

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -2,16 +2,41 @@ import React from "react"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Burger from "../components/Burger"
-import ButtonCart from "@components/ButtonCart"
 
 function Contact() {
+  const addClass = () => {
+    document.body.classList.add("sent")
+  }
+
   return (
-    <div>
+    <div className="contactBody">
       <Header />
-      <div className="contact">
-        <p>Contact</p>
-        <ButtonCart />
+      <div className="wrapper centered">
+        <article className="letter">
+          <div className="side">
+            <h1>Contactez-nous</h1>
+            <p>
+              <textarea placeholder="Votre message"></textarea>
+            </p>
+          </div>
+          <div className="side">
+            <p>
+              <input type="text" placeholder="Votre nom" />
+            </p>
+            <p>
+              <input type="email" placeholder="Votre email" />
+            </p>
+            <p>
+              <button id="sendLetter" onClick={addClass}>
+                Envoyer
+              </button>
+            </p>
+          </div>
+        </article>
+        <div className="envelope front"></div>
+        <div className="envelope back"></div>
       </div>
+      <p className="result-message centered">Merci pour votre message</p>
       <Burger />
       <Footer />
     </div>
